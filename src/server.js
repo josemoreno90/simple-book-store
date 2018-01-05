a8b72e1f1adba797c886d56ed183d091d8be459b
Jose Moreno <morenojr90@yahoo.com>
2018-01-05 10:25:41 -0800

    rewrite and refactor all original code, restructure without Router files and folder, integrate search in home view.

--- src/server.js
+++ src/server.js
@@ -1,11 +1,41 @@
 const express = require("express");
 const app = express();
-const routes = require("./server/routes")
+const bodyParser = require("body-parser")
+const books = require("./models/books.js")
 
-app.use("/", routes);
 
-const port = process.env.PORT || 3000
+app.use(bodyParser.json({ extended: true }));
 
-app.listen(port, () => {
-  console.log("Listining on port: " + port );
-});
+app.use(express.static('src/public'))
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views');
+
+
+app.get('/', (req, res, next) => {
+  books.findAll()
+  .then(books => {
+    res.render("home", {books})
+  })
+})
+
+app.get('/book-details/:book', (req, res, next) => {
+  books.findAll()
+  .then(books => {
+    res.render("home", {books})
+  })
+})
+
+app.get('/search', (req, res, next) => {
+  let searchType = req.query.searchType.toUpperCase();
+  let searchTerm = req.query.searchTerm.toUpperCase();
+  books.search(searchType, searchTerm)
+  .then((matchedBooks) => {
+    res.render("search", {matchedBooks, searchType, searchTerm})
+  })
+})
+
+
+
+
+app.listen(3000, () => console.log("Listening loud and clear"));
